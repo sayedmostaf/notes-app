ea0bb22e96b5b10a732862c318c01b5c2213d908
Sayed_Mostafa <sayedmostafa01091706102@gmail.com>
2024-08-07 15:07:16 +0300

    create edit note view

--- lib/widgets/custom_note_item.dart
+++ lib/widgets/custom_note_item.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class NoteItem extends StatelessWidget {
   const NoteItem({super.key});
@@ -7,63 +8,70 @@ class NoteItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Container(
-        padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
-        decoration: BoxDecoration(
-          color: const Color(0xffffcc80),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Stack(
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                ListTile(
-                  title: const Text(
-                    'Flutter Tips',
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 24,
-                    ),
-                  ),
-                  subtitle: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 12,
-                    ),
-                    child: Text(
-                      'Build your career with tharwat samy',
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return const EditNoteView();
+          }));
+        },
+        child: Container(
+          padding: const EdgeInsets.only(top: 24, bottom: 24, left: 16),
+          decoration: BoxDecoration(
+            color: const Color(0xffffcc80),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Stack(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  ListTile(
+                    title: const Text(
+                      'Flutter Tips',
                       style: TextStyle(
-                        color: Colors.black.withOpacity(.5),
-                        fontSize: 16,
+                        color: Colors.black,
+                        fontSize: 24,
+                      ),
+                    ),
+                    subtitle: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 12,
+                      ),
+                      child: Text(
+                        'Build your career with tharwat samy',
+                        style: TextStyle(
+                          color: Colors.black.withOpacity(.5),
+                          fontSize: 16,
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 24),
-                  child: Text(
-                    'August 6, 2024',
-                    style: TextStyle(
-                      color: Colors.black.withOpacity(.5),
-                      fontSize: 12,
+                  Padding(
+                    padding: const EdgeInsets.only(right: 24),
+                    child: Text(
+                      'August 6, 2024',
+                      style: TextStyle(
+                        color: Colors.black.withOpacity(.5),
+                        fontSize: 12,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-            Positioned(
-              top: 4,
-              right: 8,
-              child: IconButton(
-                onPressed: () {},
-                icon: const Icon(
-                  FontAwesomeIcons.xmark,
-                  size: 22,
-                  color: Colors.black,
+                ],
+              ),
+              Positioned(
+                top: 4,
+                right: 8,
+                child: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(
+                    FontAwesomeIcons.xmark,
+                    size: 22,
+                    color: Colors.black,
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
